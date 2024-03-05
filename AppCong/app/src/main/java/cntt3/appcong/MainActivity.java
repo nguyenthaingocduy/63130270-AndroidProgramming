4e1e57f45f0f9afc32b5487e52abd4216b7b0b5e
nguyenthaingocduy <87253154+nguyenthaingocduy@users.noreply.github.com>
2024-03-05 23:39:50 +0700

    update

--- AppCong/app/src/main/java/cntt3/appcong/MainActivity.java
+++ AppCong/app/src/main/java/cntt3/appcong/MainActivity.java
@@ -1,6 +1,8 @@
 package cntt3.appcong;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,26 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+//Day la bo lang nghe va xu ly su kien click len button tinhTong
+    public void XuLyCong(View view){
+        //Tim, tham chieu den dieu khien tren xml
+          EditText  editTextSoA =  findViewById(R.id.edtA);
+          EditText  editTextSoB =  findViewById(R.id.edtB);
+          EditText  editTextKetQua =  findViewById(R.id.edtKQ);
+
+//Lay du lieu ve o dieu kien so a
+String strA =     editTextSoA.getText().toString();
+//Lay du lieu ve o dieu kien so b
+String strB =     editTextSoB.getText().toString();
+//Chuyen du lieu sang dang so
+int so_A = Integer.parseInt(strA);
+int so_B = Integer.parseInt(strB);
+//Tinh toan theo yeu cau
+int tong = so_A + so_B;
+String strTong = String.valueOf(tong);//chuyen sang chuoi
+//Lay du lieu ve kq
+editTextKetQua.setText(strTong);
+
     }
 }
