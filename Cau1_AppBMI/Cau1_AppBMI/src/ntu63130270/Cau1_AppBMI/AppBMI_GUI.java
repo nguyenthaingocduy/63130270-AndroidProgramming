9a6bdff554801cc197802b3bc077c34751740e40
nguyenthaingocduy <87253154+nguyenthaingocduy@users.noreply.github.com>
2024-03-24 16:24:39 +0700

    Update

--- Cau1_AppBMI/Cau1_AppBMI/src/ntu63130270/Cau1_AppBMI/AppBMI_GUI.java
+++ Cau1_AppBMI/Cau1_AppBMI/src/ntu63130270/Cau1_AppBMI/AppBMI_GUI.java
@@ -1,5 +1,49 @@
 package ntu63130270.Cau1_AppBMI;
 
-public class AppBMI_GUI {
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
+import javax.swing.ImageIcon;
+import javax.swing.JTable;
+
+public class AppBMI_GUI extends JFrame {
+	private static final long serialVersionUID = 1L;
+	private JPanel khungNoiDung;
+	private JTextField chiSoChieuCao;
+	private JTextField chiSoCanNang;
+	private JTextField chiSoBMI;
+	//-----------------------------------------------------------------
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AppBMI_GUI frame = new AppBMI_GUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	//-------------------------------------------------------------------
+	
+	//Tao Frame
+	public a
 
 }
