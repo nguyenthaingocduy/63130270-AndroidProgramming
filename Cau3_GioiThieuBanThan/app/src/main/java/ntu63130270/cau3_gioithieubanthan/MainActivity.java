18f3a1e96399036af31d6a4e953f747f35b863a9
nguyenthaingocduy <87253154+nguyenthaingocduy@users.noreply.github.com>
2024-03-25 10:40:12 +0700

    update

--- Cau3_GioiThieuBanThan/app/src/main/java/ntu63130270/cau3_gioithieubanthan/MainActivity.java
+++ Cau3_GioiThieuBanThan/app/src/main/java/ntu63130270/cau3_gioithieubanthan/MainActivity.java
@@ -1,6 +1,9 @@
 package ntu63130270.cau3_gioithieubanthan;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+
+
+    public void button_tiktok (View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.tiktok.com/@gonudy"));
+        startActivity(intent);
+    }
+
+    public void button_fa (View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/profile.php?id=100009048886566"));
+        startActivity(intent);
+    }
+
+    public void button_github (View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/nguyenthaingocduy/63130270-AndroidProgramming"));
+        startActivity(intent);
     }
 }
