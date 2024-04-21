1ce36261f0b20b4f7e5e16feb31bc295f64dba9b
nguyenthaingocduy <87253154+nguyenthaingocduy@users.noreply.github.com>
2024-04-21 15:19:14 +0700

    update

---
+++ exam_recycleView/app/src/main/java/minhhung/handsome/exam_recycleview/examData.java
@@ -0,0 +1,13 @@
+package minhhung.handsome.exam_recycleview;
+
+public class examData {
+    String name;
+    String date;
+    String message;
+
+    examData(String name, String date, String message){
+        this.name = name;
+        this.date = date;
+        this.message = message;
+    }
+}
