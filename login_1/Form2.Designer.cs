819761ed0001b3cdd5a634f1a018e03aa37441b1
nguyenthaingocduy <87253154+nguyenthaingocduy@users.noreply.github.com>
2024-05-19 12:04:38 +0700

    update

---
+++ login_1/Form2.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace login_1
+{
+    partial class infoTextBoxes5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(infoTextBoxes5));
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.infoLabels1 = new System.Windows.Forms.Label();
+            this.infoTextBoxes1 = new System.Windows.Forms.TextBox();
+            this.infoTextBoxes2 = new System.Windows.Forms.TextBox();
+            this.infoLabels2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.infoLabels5 = new System.Windows.Forms.Label();
+            this.infoTextBoxes4 = new System.Windows.Forms.TextBox();
+            this.infoLabels4 = new System.Windows.Forms.Label();
+            this.infoTextBoxes3 = new System.Windows.Forms.TextBox();
+            this.infoLabels3 = new System.Windows.Forms.Label();
+            this.btnMessages1 = new System.Windows.Forms.Button();
+            this.btnServices1 = new System.Windows.Forms.Button();
+            this.btnChangePassword1 = new System.Windows.Forms.Button();
+            this.btnLockMachine1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserName.Location = new System.Drawing.Point(75, 25);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(123, 34);
+            this.lblUserName.TabIndex = 0;
+            this.lblUserName.Text = "HIEU113";
+            this.lblUserName.Click += new System.EventHandler(this.lblUserName_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Century Gothic", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.ForeColor = System.Drawing.Color.Lime;
+            this.lblStatus.Location = new System.Drawing.Point(232, -11);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(47, 70);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = ".";
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogout.Location = new System.Drawing.Point(423, 25);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(73, 45);
+            this.btnLogout.TabIndex = 2;
+            this.btnLogout.Text = "⟳";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
+            // 
+            // infoLabels1
+            // 
+            this.infoLabels1.AutoSize = true;
+            this.infoLabels1.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoLabels1.Location = new System.Drawing.Point(81, 108);
+            this.infoLabels1.Name = "infoLabels1";
+            this.infoLabels1.Size = new System.Drawing.Size(108, 20);
+            this.infoLabels1.TabIndex = 3;
+            this.infoLabels1.Text = "Tổng thời gian";
+            // 
+            // infoTextBoxes1
+            // 
+            this.infoTextBoxes1.Location = new System.Drawing.Point(237, 108);
+            this.infoTextBoxes1.Name = "infoTextBoxes1";
+            this.infoTextBoxes1.ReadOnly = true;
+            this.infoTextBoxes1.Size = new System.Drawing.Size(198, 22);
+            this.infoTextBoxes1.TabIndex = 4;
+            // 
+            // infoTextBoxes2
+            // 
+            this.infoTextBoxes2.Location = new System.Drawing.Point(237, 152);
+            this.infoTextBoxes2.Name = "infoTextBoxes2";
+            this.infoTextBoxes2.ReadOnly = true;
+            this.infoTextBoxes2.Size = new System.Drawing.Size(198, 22);
+            this.infoTextBoxes2.TabIndex = 6;
+            // 
+            // infoLabels2
+            // 
+            this.infoLabels2.AutoSize = true;
+            this.infoLabels2.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoLabels2.Location = new System.Drawing.Point(81, 154);
+            this.infoLabels2.Name = "infoLabels2";
+            this.infoLabels2.Size = new System.Drawing.Size(131, 20);
+            this.infoLabels2.TabIndex = 5;
+            this.infoLabels2.Text = "Thời gian sử dung";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(237, 287);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(198, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // infoLabels5
+            // 
+            this.infoLabels5.AutoSize = true;
+            this.infoLabels5.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoLabels5.Location = new System.Drawing.Point(81, 287);
+            this.infoLabels5.Name = "infoLabels5";
+            this.infoLabels5.Size = new System.Drawing.Size(117, 20);
+            this.infoLabels5.TabIndex = 7;
+            this.infoLabels5.Text = "Chi phí dịch vụ";
+            // 
+            // infoTextBoxes4
+            // 
+            this.infoTextBoxes4.Location = new System.Drawing.Point(237, 239);
+            this.infoTextBoxes4.Name = "infoTextBoxes4";
+            this.infoTextBoxes4.ReadOnly = true;
+            this.infoTextBoxes4.Size = new System.Drawing.Size(198, 22);
+            this.infoTextBoxes4.TabIndex = 10;
+            // 
+            // infoLabels4
+            // 
+            this.infoLabels4.AutoSize = true;
+            this.infoLabels4.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoLabels4.Location = new System.Drawing.Point(81, 239);
+            this.infoLabels4.Name = "infoLabels4";
+            this.infoLabels4.Size = new System.Drawing.Size(118, 20);
+            this.infoLabels4.TabIndex = 9;
+            this.infoLabels4.Text = "Chi phí sử dụng";
+            // 
+            // infoTextBoxes3
+            // 
+            this.infoTextBoxes3.Location = new System.Drawing.Point(237, 194);
+            this.infoTextBoxes3.Name = "infoTextBoxes3";
+            this.infoTextBoxes3.ReadOnly = true;
+            this.infoTextBoxes3.Size = new System.Drawing.Size(198, 22);
+            this.infoTextBoxes3.TabIndex = 12;
+            // 
+            // infoLabels3
+            // 
+            this.infoLabels3.AutoSize = true;
+            this.infoLabels3.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoLabels3.Location = new System.Drawing.Point(81, 196);
+            this.infoLabels3.Name = "infoLabels3";
+            this.infoLabels3.Size = new System.Drawing.Size(122, 20);
+            this.infoLabels3.TabIndex = 11;
+            this.infoLabels3.Text = "Thời gian còn lại";
+            // 
+            // btnMessages1
+            // 
+            this.btnMessages1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnMessages1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMessages1.Location = new System.Drawing.Point(81, 345);
+            this.btnMessages1.Name = "btnMessages1";
+            this.btnMessages1.Size = new System.Drawing.Size(160, 40);
+            this.btnMessages1.TabIndex = 13;
+            this.btnMessages1.Text = "Tin nhắn";
+            this.btnMessages1.UseVisualStyleBackColor = false;
+            this.btnMessages1.Click += new System.EventHandler(this.btnMessages1_Click);
+            // 
+            // btnServices1
+            // 
+            this.btnServices1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnServices1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnServices1.Location = new System.Drawing.Point(336, 345);
+            this.btnServices1.Name = "btnServices1";
+            this.btnServices1.Size = new System.Drawing.Size(160, 40);
+            this.btnServices1.TabIndex = 14;
+            this.btnServices1.Text = "Dịch vụ";
+            this.btnServices1.UseVisualStyleBackColor = false;
+            this.btnServices1.Click += new System.EventHandler(this.btnServices1_Click);
+            // 
+            // btnChangePassword1
+            // 
+            this.btnChangePassword1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnChangePassword1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChangePassword1.Location = new System.Drawing.Point(81, 436);
+            this.btnChangePassword1.Name = "btnChangePassword1";
+            this.btnChangePassword1.Size = new System.Drawing.Size(160, 40);
+            this.btnChangePassword1.TabIndex = 15;
+            this.btnChangePassword1.Text = "Mật khẩu";
+            this.btnChangePassword1.UseVisualStyleBackColor = false;
+            this.btnChangePassword1.Click += new System.EventHandler(this.btnChangePassword1_Click);
+            // 
+            // btnLockMachine1
+            // 
+            this.btnLockMachine1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnLockMachine1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLockMachine1.Location = new System.Drawing.Point(336, 436);
+            this.btnLockMachine1.Name = "btnLockMachine1";
+            this.btnLockMachine1.Size = new System.Drawing.Size(160, 40);
+            this.btnLockMachine1.TabIndex = 16;
+            this.btnLockMachine1.Text = "Khóa máy";
+            this.btnLockMachine1.UseVisualStyleBackColor = false;
+            this.btnLockMachine1.Click += new System.EventHandler(this.btnLockMachine1_Click);
+            // 
+            // infoTextBoxes5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 559);
+            this.Controls.Add(this.btnLockMachine1);
+            this.Controls.Add(this.btnChangePassword1);
+            this.Controls.Add(this.btnServices1);
+            this.Controls.Add(this.btnMessages1);
+            this.Controls.Add(this.infoTextBoxes3);
+            this.Controls.Add(this.infoLabels3);
+            this.Controls.Add(this.infoTextBoxes4);
+            this.Controls.Add(this.infoLabels4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.infoLabels5);
+            this.Controls.Add(this.infoTextBoxes2);
+            this.Controls.Add(this.infoLabels2);
+            this.Controls.Add(this.infoTextBoxes1);
+            this.Controls.Add(this.infoLabels1);
+            this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblUserName);
+            this.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.Name = "infoTextBoxes5";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Label infoLabels1;
+        private System.Windows.Forms.TextBox infoTextBoxes1;
+        private System.Windows.Forms.TextBox infoTextBoxes2;
+        private System.Windows.Forms.Label infoLabels2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label infoLabels5;
+        private System.Windows.Forms.TextBox infoTextBoxes4;
+        private System.Windows.Forms.Label infoLabels4;
+        private System.Windows.Forms.TextBox infoTextBoxes3;
+        private System.Windows.Forms.Label infoLabels3;
+        private System.Windows.Forms.Button btnMessages1;
+        private System.Windows.Forms.Button btnServices1;
+        private System.Windows.Forms.Button btnChangePassword1;
+        private System.Windows.Forms.Button btnLockMachine1;
+    }
+}
---
+++ login_1/Form2.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace login_1
+{
+    public partial class infoTextBoxes5 : Form
+    {
+    
+        private Label[] infoLabels;
+        private TextBox[] infoTextBoxes;
+        private Button btnMessages;
+        private Button btnServices;
+        private Button btnChangePassword;
+        private Button btnLockMachine;
+
+        public infoTextBoxes5()
+        {
+            InitializeComponent();
+        }
+
+        private void lblUserName_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Logout button clicked");
+        }
+        private void btnMessages1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Messages button clicked");
+        }
+
+        private void btnServices1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Services button clicked");
+        }
+
+        private void btnChangePassword1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Password button clicked");
+        }
+
+        private void btnLockMachine1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Lock Machine button clicked");
+        }
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form());
+        }
+        private void InitializeComponent1()
+        {
+            this.lblUserName = new Label();
+            this.lblStatus = new Label();
+            this.btnLogout = new Button();
+
+            // Set properties and add controls
+            // User Label
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Font = new Font("Arial", 18F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserName.Location = new Point(20, 20);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new Size(100, 29);
+            this.lblUserName.Text = "HIEU123";
+            this.Controls.Add(this.lblUserName);
+            // Status Indicator
+            lblStatus = new Label
+            {
+                Text = "●",
+                ForeColor = Color.Green,
+                Location = new Point(150, 28),
+                AutoSize = true
+            };
+            this.Controls.Add(lblStatus);
+            // Logout Button
+            this.btnLogout.Font = new Font("Arial", 14F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogout.Location = new Point(350, 20);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new Size(30, 30);
+            this.btnLogout.Text = "⟳";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            this.btnLogout.Click += new EventHandler(this.btnLogout_Click);
+            this.Controls.Add(this.btnLogout);
+
+            // Information Labels and TextBoxes
+            string[] infoTexts = { "Tổng thời gian", "Thời gian sử dụng", "Thời gian còn lại", "Chi phí sử dụng", "Chi phí dịch vụ" };
+            this.infoLabels = new Label[infoTexts.Length];
+            this.infoTextBoxes = new TextBox[infoTexts.Length];
+
+            int yPosition = 70;
+            for (int i = 0; i < infoTexts.Length; i++)
+            {
+                this.infoLabels[i] = new Label
+                {
+                    Text = infoTexts[i],
+                    Location = new Point(20, yPosition),
+                    AutoSize = true
+                };
+                this.Controls.Add(this.infoLabels[i]);
+
+                this.infoTextBoxes[i] = new TextBox
+                {
+                    Location = new Point(150, yPosition - 4),
+                    Size = new Size(200, 22),
+                    ReadOnly = true,
+                    BackColor = Color.LightGray
+                };
+                this.Controls.Add(this.infoTextBoxes[i]);
+
+                yPosition += 40;
+            }
+            // Buttons
+            btnMessages = new Button
+            {
+                Text = "Tin nhắn",
+                Location = new Point(20, yPosition + 20),
+                Size = new Size(160, 40),
+                BackColor = Color.LightGray
+            };
+            btnMessages.Click += btnMessages1_Click;
+            this.Controls.Add(btnMessages);
+
+            btnServices = new Button
+            {
+                Text = "Dịch vụ",
+                Location = new Point(200, yPosition + 20),
+                Size = new Size(160, 40),
+                BackColor = Color.LightGray
+            };
+            btnServices.Click += btnServices1_Click;
+            this.Controls.Add(btnServices);
+
+            btnChangePassword = new Button
+            {
+                Text = "Mật khẩu",
+                Location = new Point(20, yPosition + 80),
+                Size = new Size(160, 40),
+                BackColor = Color.YellowGreen
+            };
+            btnChangePassword.Click += btnChangePassword1_Click;
+            this.Controls.Add(btnChangePassword);
+
+            btnLockMachine = new Button
+            {
+                Text = "Khóa máy",
+                Location = new Point(200, yPosition + 80),
+                Size = new Size(160, 40),
+                BackColor = Color.GreenYellow
+            };
+            btnLockMachine.Click += btnLockMachine1_Click;
+            this.Controls.Add(btnLockMachine);
+        }
+
+
+    }
+
+
+}
+
