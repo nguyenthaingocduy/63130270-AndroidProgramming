29f0ccfdaa5b079cb3cb2be48361e8d2713c772b
nguyenthaingocduy <87253154+nguyenthaingocduy@users.noreply.github.com>
2024-05-23 11:24:37 +0700

    update

--- UngDungSucKhoe/app/src/main/java/didi63cntt/ungdungsuckhoe/Activity/MainActivity.java
+++ UngDungSucKhoe/app/src/main/java/didi63cntt/ungdungsuckhoe/Activity/MainActivity.java
@@ -7,20 +7,52 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
+import didi63cntt.ungdungsuckhoe.Domain.Category_Domain;
 import didi63cntt.ungdungsuckhoe.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView.Adapter catAdapter, bestDealAdapter;
+    private RecyclerView recyclerViewCat, recyclerViewBestDeal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
+
+
+        initRecyclerViewCat();
+
+
+
+
+
+
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+
+    private void initRecyclerViewCat(){
+        ArrayList<Category_Domain> items = new ArrayList<>();
+        items.add(new Category_Domain("cat1", "Vegetable"));
+        items.add(new Category_Domain("cat2", "Fruits"));
+        items.add(new Category_Domain("cat3", "Dairy"));
+        items.add(new Category_Domain("cat4", "Drinks"));
+        items.add(new Category_Domain("cat5", "Grain"));
+    }
+
 }
--- UngDungSucKhoe/app/src/main/java/didi63cntt/ungdungsuckhoe/Adapter/CategoryAdapters.java
+++ UngDungSucKhoe/app/src/main/java/didi63cntt/ungdungsuckhoe/Adapter/CategoryAdapters.java
@@ -1,5 +1,7 @@
 package didi63cntt.ungdungsuckhoe.Adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -8,24 +10,43 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import didi63cntt.ungdungsuckhoe.Domain.Category_Domain;
 import didi63cntt.ungdungsuckhoe.R;
 
 public class CategoryAdapters extends RecyclerView.Adapter<CategoryAdapters.viewholder> {
+    ArrayList<Category_Domain> items;
+    Context context;
+    public CategoryAdapters(ArrayList<Category_Domain> items) {
+        this.items = items;
+    }
+
 
     @NonNull
     @Override
     public CategoryAdapters.viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        context=parent.getContext();
+        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_category, parent, false);
+        return new viewholder(inflate);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CategoryAdapters.viewholder holder, int position) {
+        holder.titleTxt.setText(items.get(position).getTitle());
+        int drawableResourceId = holder.itemView.getResources()
+                .getIdentifier(items.get(position).getImgPath(),"drawable", holder.itemView.getContext().getPackageName());
 
+        Glide.with(context)
+                .load(drawableResourceId)
+                .into(holder.pic);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return items.size();
     }
 
     public class viewholder extends RecyclerView.ViewHolder {
