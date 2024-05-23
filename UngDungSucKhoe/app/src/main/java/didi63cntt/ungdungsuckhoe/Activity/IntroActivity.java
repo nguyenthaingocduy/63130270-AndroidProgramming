5e6c434a3d145bb162f5e2fdab4bf0f05de8aa0b
nguyenthaingocduy <87253154+nguyenthaingocduy@users.noreply.github.com>
2024-05-23 10:02:45 +0700

    update

--- UngDungSucKhoe/app/src/main/java/didi63cntt/ungdungsuckhoe/Activity/IntroActivity.java
+++ UngDungSucKhoe/app/src/main/java/didi63cntt/ungdungsuckhoe/Activity/IntroActivity.java
@@ -1,6 +1,8 @@
-package didi63cntt.ungdungsuckhoe;
+package didi63cntt.ungdungsuckhoe.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +10,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class IntroActivity extends AppCompatActivity {
+import didi63cntt.ungdungsuckhoe.R;
 
+public class IntroActivity extends AppCompatActivity {
+    private Button introBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+
+        introBtn= findViewById(R.id.introBtn);
+        introBtn.setOnClickListener(v -> startActivity(new Intent(IntroActivity.this,MainActivity.class)));
+
+
+
+
+
+
+
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
